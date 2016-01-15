3c5a8ad7829530bfded75e52fbbe85dcc8a1989c
kmein <kieran.meinhardt@gmail.com>
2016-01-15 15:21:16 +0100

    Added Text datatype option, html text can now be added

--- Language/CML/Expression.hs
+++ Language/CML/Expression.hs
@@ -4,7 +4,8 @@ module Language.CML.Expression where
 import Text.ParserCombinators.Parsec
 
 data Expr = Expr String [Spec] [Expr] -- ^ The elementary expression type is a tag-name with zero to possible infinite specifications (classes, ids, other attributes) and child expressions.
-  deriving (Eq, Show)
+          | Text String               -- ^ Source text (content) which is not a tag.
+          deriving (Eq, Show)
 
 data Spec = Id String             -- ^ A data type for <tag id="id-name"></tag>.
           | Attr (String, String) -- ^ A data type for <tag key="value"></tag>.
@@ -17,10 +18,11 @@ braced p = char '{' *> optional spaces *> p <* optional spaces <* char '}'
 
 -- | The main parser: parses an entire HTML tag tree.
 parseExpr :: Parser Expr
-parseExpr = Expr
-         <$> parseTag
-         <*> many parseSpec <* optional spaces
-         <*> braced (parseExpr `sepBy` optional spaces)
+parseExpr = try parseSubtree <|> (Text <$> parseText)
+  where
+    parseSubtree = Expr <$> parseTag
+                <*> many parseSpec <* optional spaces
+                <*> braced (parseExpr `sepBy` optional spaces)
 
 -- | Parses a tag name.
 parseTag :: Parser String
@@ -31,12 +33,14 @@ parseSpec :: Parser Spec
 parseSpec = parseId <|> parseClass <|> parseAttr
   where
     parseId, parseClass, parseAttr :: Parser Spec
-    parseId = Id <$> (char '#' *> parseIdent)
+    parseId    = Id <$> (char '#' *> parseIdent)
     parseClass = Class <$> (char '.' *> parseIdent)
-    parseAttr = fmap Attr $ (,) <$> (char ':' *> parseIdent <* char '=') <*> parseString
+    parseAttr  = fmap Attr $ (,) <$> (char ':' *> parseIdent <* char '=') <*> parseString
 
 -- | Simple string and identifier parsers.
-parseIdent, parseString :: Parser String
-parseIdent = (:) <$> (lower <|> upper) <*> many (alphaNum <|> oneOf "-_")
+parseIdent, parseString, parseText :: Parser String
+parseIdent  = (:) <$> (lower <|> upper) <*> many (alphaNum <|> oneOf "-_")
 parseString = char '"' *> many (noneOf ['"']) <* char '"'
+parseText   = many1 (noneOf "{}")
 
+testParse = parse parseExpr []
